40773639851e9f0f5ccc96fd4503b627a006ba31
Trvpboi <129084610+Trvpboi@users.noreply.github.com>
2024-05-13 01:15:56 +0300

    Add files via upload

---
+++ porject1/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner data=new Scanner(System.in);
+        int num1=data.nextInt();
+        System.out.println(num1);
+    }
+}
